2d324d376b3cc4c39f33dea0babe14011a8e9e3e
jhharwood <jhharwood@gmail.com>
2018-08-15 21:28:35 -0400

    Updated output file name

--- readSbet/main.cpp
+++ readSbet/main.cpp
@@ -6,9 +6,10 @@
 #include <cstdio>
 #include <iostream>
 #include <iomanip>
-
+using namespace std;
 //#define FILE_NAME "sbet_RTX_NAD83_180808_1742_a.out"
-#define OUT_FILE "sbet_RTX_NAD83_180808_1742_a.txt"
+#define OUT_FILE "sbet_out.txt"
+
 
 // Define a structure to read in the sbet
 // Define a sbet record class
@@ -46,13 +47,13 @@ class SbetRecord
 
         void displayRecords()
         {
-            std::cout << std::fixed << std::setprecision(17) << "Time:   " << records.time << "\n";
-            std::cout << std::fixed << std::setprecision(17) << "Latitude:  " << radian_to_degrees(records.lat) << "\n";
-            std::cout << std::fixed << std::setprecision(17) << "Longitude: " << radian_to_degrees(records.lon) << "\n";
-            std::cout << std::fixed << std::setprecision(17) << "Altitude: " << records.alt << "\n";
-            std::cout << std::fixed << std::setprecision(17) << "Roll: " << records.roll << "\n";
-            std::cout << std::fixed << std::setprecision(17) << "Pitch: " << records.pitch << "\n";
-            std::cout << std::fixed << std::setprecision(17) << "Heading: " << records.heading << "\n";
+            cout << fixed << setprecision(17) << "Time:   " << records.time << "\n";
+            cout << fixed << setprecision(17) << "Latitude:  " << radian_to_degrees(records.lat) << "\n";
+            cout << fixed << setprecision(17) << "Longitude: " << radian_to_degrees(records.lon) << "\n";
+            cout << fixed << setprecision(17) << "Altitude: " << records.alt << "\n";
+            cout << fixed << setprecision(17) << "Roll: " << records.roll << "\n";
+            cout << fixed << setprecision(17) << "Pitch: " << records.pitch << "\n";
+            cout << fixed << setprecision(17) << "Heading: " << records.heading << "\n";
         }
 
         double getTime()
@@ -92,6 +93,23 @@ class SbetRecord
 
 };
 
+string getFileName(const string &s)
+    {
+        char sep = '/';
+
+    #ifdef _WIN32
+        sep = '\\';
+    #endif
+
+        size_t i = s.rfind(sep, s.length());
+        if (i != string::npos)
+        {
+          return(s.substr(i+1, s.length() - i));
+        }
+
+        return("");
+    }
+
 // Start Main
 int main(int argc, const char* argv[])
 //int main()
@@ -100,60 +118,72 @@ int main(int argc, const char* argv[])
     if (argc < 2)
     {
         // Tell the user how to run the program
-        std::cerr << "Usage: readSbet [sbet name]\n"
+        cerr << "Usage: readSbet [sbet name]\n"
                   << "Sbet name should be path to the sbet *.out file\n"
-                  << "Example Usage: readSbet.exe D:/path/to/myfile.out\n"
-                  << std::endl;
+                  << "Example Usage: readSbet.exe D:/path/to/sbet*.out\n"
+                  << endl;
         return 1;
     }
 
-    std::clock_t start;
+    // Define time processing variables
+    clock_t start;
     int duration;
 
-    start = std::clock();
+    // Start the timer to get the processing time.
+    start = clock();
 
     // Read/Write Files
-    //const char *input = "D:/myIn.tif"
-    const char *input = argv[1];
-    std::cout << "This is your file " << input << std::endl;
+
+
+    // get the sbet from the command execution
+    const char *sbet = argv[1];
+    //string path = argv[1];
+    //char filename;
+    cout << "This is your sbet file " << sbet << endl;
 
     SbetRecord record;
 
-    // Open the input file;
-    std::ifstream file (input, std::ios::in | std::ios::binary);
-    if(!file.is_open())
+    // Open the sbet and exif file;
+    ifstream fileSbet (sbet, ios::in | ios::binary);
+
+    if(!fileSbet.is_open() && !fileSbet)
     {
-        std::cout << "Error in file opening..." << std::endl;
+        cout << "Error in file opening..." << endl;
         return 0;
     }
     else
     {
-        std::cout << "Opening file: " << input << std::endl;
+        cout << "Opening file: " << sbet << endl;
     }
 
+    // Define Sbet out variable
+    string sbetName;
+    //sbetName = getFileName(path);
+    //ofstream outTxt;
+    cout << "This the sbet " << sbet << endl;
 
-    std::ofstream out (OUT_FILE, std::ios::out | std::ios::app);
-    while (!file.eof())
+    ofstream outTxt (OUT_FILE, ios::out | ios::app);
+    while (!fileSbet.eof())
     {
-        file.read((char *)&record, sizeof(record));
+        fileSbet.read((char *)&record, sizeof(record));
         // Print record information
         //record.displayRecords();
-        //std::cout << "\n";
 
-        //out.write((char*)&record,sizeof(record));
-        //out << record.time << "," << record.lat << "," << record.lon << record.alt << "\n";
-
-        out << std::fixed << std::setprecision(17) << record.getTime() << "," << record.radian_to_degrees(record.getLat()) << ","   \
+        outTxt << fixed << setprecision(17) << record.getTime() << "," << record.radian_to_degrees(record.getLat()) << ","   \
         << record.radian_to_degrees(record.getLon()) << "," << record.getAlt() << "," <<  record.getRoll() << "," << record.getPitch() << "," \
-        << record.getHeading() << std::endl;
+        << record.getHeading() << endl;
 
     }
 
-    duration = ( std::clock() - start ) / (int) CLOCKS_PER_SEC;
-    std::cout << "Processing time is: " << duration  << " seconds." << std::endl;
+    // Tell user file has been read and is closing
+    cout << "Closing file: " << sbet << endl;
 
-    file.close();
-    out.close();
+    // Output time to read and process file
+    duration = ( clock() - start ) / (int) CLOCKS_PER_SEC;
+    cout << "Processing time is: " << duration  << " seconds." << endl;
+
+    fileSbet.close();
+    outTxt.close();
 
     return 0;
 }
